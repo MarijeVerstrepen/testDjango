69a252f19bccd462780ea6dee456608bf1bcc315
MarijeVerstrepen <marijeverstrepen@gmail.com>
2025-08-13 18:40:23 +0200

    Huiswerk les 6

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # Include URLs from myapp
+    path('polls/', include('polls.urls')),  # Include URLs from polls app
 ]
